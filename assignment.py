6ada97060546c079d7ebfd6522335c8c20596b3a
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-11-10 23:40:32 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,14 +1,64 @@
-# Exercise 1
-class Library:
-    pass
+# solutions.py
 
-# Exercise 2
-class Player:
-    pass
+class Stack:
+    def __init__(self):
+        self.items = []
 
-# Exercise 3
-class Song:
-    pass
+    def push(self, item):
+        self.items.append(item)
 
-class Playlist:
-    pass
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        return None
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+
+def decimal_to_binary(n):
+    stack = Stack()
+
+    if n == 0:
+        return "0"
+
+    while n > 0:
+        remainder = n % 2
+        stack.push(remainder)
+        n //= 2
+
+    binary = ""
+    while not stack.is_empty():
+        binary += str(stack.pop())
+
+    return binary
+
+
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        return None
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+
+def bank_simulation(customers):
+    q = Queue()
+
+    for name in customers:
+        q.enqueue(name)
+
+    output = ""
+    while not q.is_empty():
+        output += f"Serving: {q.dequeue()}\n"
+
+    return output.strip()
