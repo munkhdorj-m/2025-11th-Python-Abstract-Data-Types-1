5b65232463c6527a30d7c8686ef152e704464e94
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-11-10 23:43:34 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,64 +1,18 @@
-# solutions.py
-
+# Exercise 1
 class Stack:
-    def __init__(self):
-        self.items = []
-
-    def push(self, item):
-        self.items.append(item)
-
-    def pop(self):
-        if not self.is_empty():
-            return self.items.pop()
-        return None
-
-    def is_empty(self):
-        return len(self.items) == 0
+    pass
 
 
 def decimal_to_binary(n):
-    stack = Stack()
-
-    if n == 0:
-        return "0"
-
-    while n > 0:
-        remainder = n % 2
-        stack.push(remainder)
-        n //= 2
-
-    binary = ""
-    while not stack.is_empty():
-        binary += str(stack.pop())
-
-    return binary
+    # complete this function
+    pass
 
 
-
+# Exercise 2
 class Queue:
-    def __init__(self):
-        self.items = []
-
-    def enqueue(self, item):
-        self.items.append(item)
-
-    def dequeue(self):
-        if not self.is_empty():
-            return self.items.pop(0)
-        return None
-
-    def is_empty(self):
-        return len(self.items) == 0
+    pass
 
 
 def bank_simulation(customers):
-    q = Queue()
-
-    for name in customers:
-        q.enqueue(name)
-
-    output = ""
-    while not q.is_empty():
-        output += f"Serving: {q.dequeue()}\n"
-
-    return output.strip()
+    #complete this function
+    pass
